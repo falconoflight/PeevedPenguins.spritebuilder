2f2fdb0f0176e6cf20440c4afc76ff833a9ec39f
Laura Bennett <falconoflight@gmail.com>
2015-07-09 12:58:05 -0700

    broken near finished no iPad

---
+++ Source/Penguin.swift
@@ -0,0 +1,13 @@
+//
+//  Penguin.swift
+//  PeevedPenguins
+//
+//  Created by Carol on 6/29/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+import UIKit
+
+class Penguin: CCSprite {
+   
+}
---
+++ Source/WaitingPenguin.swift
@@ -0,0 +1,13 @@
+//
+//  WaitingPenguin.swift
+//  PeevedPenguins
+//
+//  Created by Carol on 7/9/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+import Cocoa
+
+class WaitingPenguin: CCSprite {
+
+}
